16be63de5ac6a6402ccfccdbfff65a8193849036
Jhamyl Tukhvatullin <sinkler86@gmail.com>
2022-03-30 08:49:26 +0700

    initisl commit

---
+++ src/test/java/ru/netology/SQRServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SQRServiceTest {
+
+    @Test
+    public void shouldOccurrenceOfCount() {
+
+        int minRange = 200;
+        int maxRange = 300;
+
+        SQRService counter = new SQRService();
+        System.out.println("Количество квадратов: "
+                + counter.occurrenceOfCount(minRange, maxRange));
+
+        int actual = counter.occurrenceOfCount(minRange, maxRange);
+
+        int expected = 3;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldOccurrenceOfDifferentCount() {
+
+        int minRange = 10;
+        int maxRange = 300;
+
+        SQRService counter = new SQRService();
+        System.out.println("Количество квадратов: "
+                + counter.occurrenceOfCount(minRange, maxRange));
+
+        int actual = counter.occurrenceOfCount(minRange, maxRange);
+
+        int expected = 8;
+        assertEquals(expected, actual);
+    }
+}
